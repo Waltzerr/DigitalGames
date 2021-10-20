9152f365e4465e79c39c69c663650eb0035d0329
Ben Holmes <benholmesy980@gmail.com>
2021-10-20 15:24:53 +1100

    Settings panel works, opens and closes with the buttons, looks bad though

--- Scripts/MenuManager.cs
+++ Scripts/MenuManager.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject panel;
+
     public void PlayButton()
     {
         SceneManager.LoadScene("Game");
+    }
+
+    public void SettingsButton()
+    {
+        panel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        panel.SetActive(false);
     }
 }
