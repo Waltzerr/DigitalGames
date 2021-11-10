cb80ce31e5451258ec78e48805bda608862e4c20
Ben Holmes <benholmesy980@gmail.com>
2021-11-10 12:08:32 +1100

    Game over scene added

---
+++ Scripts/GameOverManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverManager : MonoBehaviour
+{
+    
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ReplayButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
